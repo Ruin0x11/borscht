f6687ff2aa592b4b0382751d09a008e457dc9b57
Ruin0x11 <ipickering2@gmail.com>
2021-08-02 15:29:11 -0700

    Fix color

--- analyze.sh
+++ analyze.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
 cargo run --release -- analyze -s -o ../custom-gx/1.90-borscht ../elonaplus1.90/start.ax
 cargo run --release -- analyze -s -o ../custom-gx/2.05-borscht ../elonaplus2.05/elonaplus.ax
 cargo run --release -- print-vars database/plus1.90.ron > ../custom-gx/1.90.hsp
 cargo run --release -- print-vars database/plus2.05.ron > ../custom-gx/2.05.hsp
+
+set +e
 
 unix2dos ../custom-gx/1.90-borscht/*.hsp
 unix2dos ../custom-gx/2.05-borscht/*.hsp
